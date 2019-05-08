1d6d069494482fea4b4ca095848682007d04af30
kailucho <luijhy234@gmail.com>
2019-05-08 09:22:08 -0500

    modificacion de menu

--- src/js/components/Obras/HistorialObra.js
+++ src/js/components/Obras/HistorialObra.js
@@ -100,35 +100,47 @@ class HistorialObra extends Component {
 
   
     handleSubmit(event) {
-        event.preventDefault();
+        // event.preventDefault();
         var dataHistorial=this.state.dataHistorial
         dataHistorial[dataHistorial.length-1][3] = "2030-12-01"
         for(var i=0;i<dataHistorial.length-1;i++){
             dataHistorial[i][3] =  dataHistorial[i+1][2]
         }
-        // var id_actualizacion = 0
-        // //encontrar el id estado de obra actualizacion
-        // for (let i = 0; i < this.state.estados.length; i++) {
-        //     if(this.state.estados[i].nombre =="Actualizacion"){
-        //         id_actualizacion = this.state.estados[i].id_Estado;
-        //         break;
-        //     }
-        // }
-        // console.log("id_actualizacion",id_actualizacion);
+        var id_actualizacion = 0
+        //encontrar el id estado de obra actualizacion
+        for (let i = 0; i < this.state.estados.length; i++) {
+            if(this.state.estados[i].nombre =="Actualizacion"){
+                id_actualizacion = this.state.estados[i].id_Estado;
+                break;
+            }
+        }
+        console.log("id_actualizacion",id_actualizacion);
         
-        // //resion de estado actualizacion
-        // var fecha_final = "2030-12-01"
-        // console.log(dataHistorial);
-        // var act  = false
-        // for(var i= dataHistorial.length-1; i>=0;i--){
-        //     if(dataHistorial[i][4] != id_actualizacion && act){
-        //         dataHistorial[i][3] = fecha_final
-        //         act = false
-        //     }else{
-        //         fecha_final = dataHistorial[i][3]
-        //         act = true
-        //     }
-        // }
+        //resion de estado actualizacion
+        var fecha_final = "2030-12-01"
+        console.log(dataHistorial);
+        var act  = false
+        for(var i= dataHistorial.length-1; i>=0;i--){
+            if(dataHistorial[i][4] != id_actualizacion && act){
+                console.log(1,act);
+                dataHistorial[i][3] = fecha_final
+                act = false
+            }else if(dataHistorial[i][4] == id_actualizacion && !act ){
+                console.log(2,act);
+                fecha_final = dataHistorial[i][3]
+                act = true
+
+            }else if(dataHistorial[i][4] == id_actualizacion && act ){
+                console.log(3,act);
+                dataHistorial[i][3] = fecha_final
+                act = true
+
+            }else{
+                console.log(4,act);
+                // dataHistorial[i][3] = fecha_final
+
+            }
+        }
         console.log(dataHistorial);
         axios.post(`${UrlServer}/postHistorialEstadosObra`,
             dataHistorial
