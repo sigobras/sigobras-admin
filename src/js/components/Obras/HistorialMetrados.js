5272837391f21646deaaca1ed18c151602826f08
kailucho <luijhy9234@gmail.com>
2020-02-02 10:32:55 -0500

    anyo 2020

--- src/js/components/Obras/HistorialMetrados.js
+++ src/js/components/Obras/HistorialMetrados.js
@@ -58,6 +58,7 @@ class HistorialMetrados extends Component {
                         <label>Seleccione el anio</label>
                         <select className="form-control" value={this.state.anio} id="anio" onChange={this.handleChange}>
                             <option value="0">todos</option>
+                            <option value="2020">2020</option>
                             <option value="2019">2019</option>
                             <option value="2018">2018</option>
                             <option value="2017">2017</option>
