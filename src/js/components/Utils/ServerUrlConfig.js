21f45c26a61fbd9e47a372a997769e87e37005ca
kaido <luijhy9234@gmail.com>
2023-04-01 23:56:24 -0500

    cambio de api

--- src/js/components/Utils/ServerUrlConfig.js
+++ src/js/components/Utils/ServerUrlConfig.js
@@ -1,7 +1,7 @@
 // para desarrollo con el backend local
 // const UrlServer = "http://localhost:9000";
 // //para git pull cambiar a este modo
-const UrlServer = "https://api.sigobras.com";
+const UrlServer = "https://brzhxn1b16.execute-api.us-east-1.amazonaws.com/";
 const Id_Acceso = sessionStorage.getItem("idacceso");
 module.exports = {
   UrlServer,
